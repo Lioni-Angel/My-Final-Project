823a8053fa629480bbe24db4a72f4e34a5d360cd
Lioni-Angel <hermeni259@gmail.com>
2020-05-06 19:59:35 +0300

    Lioni

--- My Final Project/My_Final_Project/views.py
+++ My Final Project/My_Final_Project/views.py
@@ -5,6 +5,44 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from My_Final_Project import app 
+from My_Final_Project.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError 
+
+
+from My_Final_Project.Models.QueryFormStructure import QueryFormStructure 
+from My_Final_Project.Models.QueryFormStructure import LoginFormStructure 
+from My_Final_Project.Models.QueryFormStructure import UserRegistrationFormStructure 
+
+###from DemoFormProject.Models.LocalDatabaseRoutines import IsUserExist, IsLoginGood, AddNewUser 
+
+db_Functions = create_LocalDatabaseServiceRoutines() 
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -39,11 +77,16 @@ def about():
 @app.route('/DataBase')
 def data():
     """Renders the about page."""
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\2018 FIFA World Cup Squads.csv'))
+    raw_data_table = df.to_html(classes = 'table table-hover') 
+
     return render_template(
         'data.html',
         title='My Data Base Page~ ',
         year=datetime.now().year,
-        message='Here you can find a link for the 2018 FIFA World Cup Squads Data Base'
+        message='Here you can find a link for the 2018 FIFA World Cup Squads Data Base', 
+
+        raw_data_table = raw_data_table, 
     )
 
 @app.route('/photoAlbum')
@@ -56,14 +99,44 @@ def photoAlbum():
         message='warning: might be too cute'
     )
 
-
-@app.route('/registar')
+@app.route('/registar', methods=['GET', 'POST'])
 def registar():
-    """Renders the contact page."""
-    return render_template(
-        'registar.html',
-        title='Here you can easly registar for my website!',
-        year=datetime.now().year,
-        message='You just need to fill in the details' 
-    )
+    form = UserRegistrationFormStructure(request.form)
 
+    if (request.method == 'POST' and form.validate()): 
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)): 
+            flash('Login approved!') 
+            #return redirect('<where to go if login is good!') 
+        else:
+            flash('Error in - Username and/or password') 
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
--- My Final Project/runserver.py
+++ My Final Project/runserver.py
@@ -8,7 +8,12 @@ from My_Final_Project import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5555')) 
+
     except ValueError:
-        PORT = 5555
+        PORT = 5555 
+
+        
+    app.config['SECRET_KEY'] = 'All You Need Is Love Ta ta ta ta ta'
     app.run(HOST, PORT)
+
